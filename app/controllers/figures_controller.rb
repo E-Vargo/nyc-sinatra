050755f207d48aa49df995fb4484e9258eb24283
E-Vargo <vargoelliott@gmail.com>
2022-01-15 12:32:31 -0800

    commit tree d6782138fb9bd84e76365eedf77908597535f69e

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,52 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all 
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do 
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do 
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do 
+    @titles = Title.all
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures/new' do 
+    @figures = Figure.all
+    @figure = Figure.create(params[:figure])
+
+    if params[:title][:name] == "" && params[:landmark][:name] != ""
+      @figure.landmarks << Landmark.create(params[:landmark])
+    elsif params[:title][:name] != "" && params[:landmark][:name] != ""
+      @figure.titles << Title.create(params[:title])
+    else 
+      @figure.landmarks << Landmark.create(params[:landmark])
+      @figure.titles << Title.create(params[:title])
+    end
+    erb :'figures/index'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    hash = {:name => params[:title]}
+    unless hash[:name].empty?
+      @figure.titles << Title.create(hash)
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
